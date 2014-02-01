e952542aac89d448253eea9c1e1bdbb75c31c894
Landen Samantha <slanden@andrew.cmu.edu>
2014-02-01 15:11:15 -0500

    changed source for dumb files"

--- app/app.js
+++ app/app.js
@@ -31,7 +31,7 @@ passport.deserializeUser(function(obj, done) {
 passport.use(new FacebookStrategy({
     clientID: FACEBOOK_APP_ID,
     clientSecret: FACEBOOK_APP_SECRET,
-    callbackURL: "http://localhost:3000/auth/facebook/callback"
+    callbackURL: "localhost:3000/auth/facebook/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     // asynchronous verification, for effect...
